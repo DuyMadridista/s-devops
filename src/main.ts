5dfa2f4376b13e48e19faaf1d3f54aec85d5deb7
duylv <vanduyluu432003@gmail.com>
2024-06-21 14:25:38 +0700

    add ci-cd file

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Request, Response } from 'express'; // Import the Request and Response types from 'express'
 
 async function bootstrap() {
-  const PORT = process.env.PORT || 3000;
+  const PORT = 3001;
   const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
 
+  app.getHttpAdapter().get('/api/simple-endpoint', (req: Request, res: Response) => {
+    return res.send('This is a simple endpoint!');
+  });
   await app.listen(PORT, () => console.log(`App is running on port: ${PORT}`));
 }
 
